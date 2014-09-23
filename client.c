860c24c528e697b94d71e838d88c1f18a63a4de3
Alexander Standke <xanderstrike@gmail.com>
2014-09-23 11:47:15 -0700

    make work lol

--- client.c
+++ client.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 
@@ -22,33 +25,40 @@ int main(int argc, char *argv[])
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    struct timeval wait_time;
 
     char buffer[256];
+    
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
     if (sockfd < 0) 
         error("ERROR opening socket");
+    
     server = gethostbyname(argv[1]);
+    
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+    
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
+    
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
     n = read(sockfd, buffer, 255);
     printf("%s\n",buffer);
-    bzero(buffer,256);
 
     printf("Please enter your name: ");
     bzero(buffer,256);
@@ -58,15 +68,36 @@ int main(int argc, char *argv[])
          error("ERROR writing to socket");
     bzero(buffer,256);
 
-    while ( (n = read(sockfd, buffer, 255)) > 0 ) {
+    wait_time.tv_sec = 0;    
+    wait_time.tv_usec = 7500;    
+    select(32, NULL, NULL, NULL, &wait_time);
+    n = read(sockfd, buffer, 255);
+    printf("%s\n",buffer);
+    bzero(buffer,256);
+
+    while ( 1 == 1 ) {
+
+        // Check and see if there is data waiting to be read
+        int count;
+        ioctl(sockfd, FIONREAD, &count);
+
+        // If there's new data, read it
+        if (count != 0) {
+            bzero(buffer,256);
+            n = read(sockfd, buffer, 255);
+            printf("%s\n",buffer);
+        }
         
-        printf("%s\n",buffer);
-        
+        // Collect a new message
         printf("> ");
         bzero(buffer,256);
         fgets(buffer,255,stdin);
         if (strlen(buffer) > 1) { // only write message if it contains more than a newline
             n = write(sockfd,buffer,strlen(buffer));
+            bzero(buffer,256);
+            wait_time.tv_sec = 0;    
+            wait_time.tv_usec = 7500;  
+            select(32, NULL, NULL, NULL, &wait_time);
         }
     }
 
